e6ae6fc01968ab3be3688f23535dd3b44ee81ca1
unknown <byadiso@gmail.com>
2023-05-27 10:34:50 +0200

    adding-logic-to-open-tab-with classes-and-id

--- public/js/my_profile.js
+++ public/js/my_profile.js
@@ -1,11 +1,47 @@
 /* eslint-disable prettier/prettier */
 document.addEventListener('DOMContentLoaded', () => {
-    var dropdown = document.getElementsByClassName('dropdown')
-    var list = document.getElementsByClassName('main_rents')
+    var tabs = document.getElementsByClassName('tab')
+    var tabContents = document.getElementsByClassName('tab-content')
+    var profile_details = document.getElementById('profile_details')
+    var membership_details = document.getElementById('membership_details')
+    var ads_details = document.getElementById('ads_details')
+    var inbox_details = document.getElementById('inbox_details')
+
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', (e) => {
+            if (e.target.dataset.type === 'profile') {
+                profile_details.style.display = 'block'
+                inbox_details.style.display = 'none'
+                ads_details.style.display = 'none'
+                membership_details.style.display = 'none'
+            }
+            if (e.target.dataset.type === 'membership') {
+                membership_details.style.display = 'block'
+                profile_details.style.display = 'none'
+                inbox_details.style.display = 'none'
+                ads_details.style.display = 'none'
+
+                console.log('let add some profile content')
+            }
+            if (e.target.dataset.type === 'ads') {
+                ads_details.style.display = 'block'
+                profile_details.style.display = 'none'
+                inbox_details.style.display = 'none'
+                membership_details.style.display = 'none'
+                console.log('let add some profile content')
+            }
+
+            if (e.target.dataset.type === 'inbox') {
+                inbox_details.style.display = 'block'
+                profile_details.style.display = 'none'
+                ads_details.style.display = 'none'
+                membership_details.style.display = 'none'
+                console.log('let add some profile content')
+            }
+        })
+    }
 
     function changeTab(tabIndex) {
-        var tabs = document.getElementsByClassName('tab')
-        var tabContents = document.getElementsByClassName('tab-content')
         var selectedTab = document.getElementsByClassName('tab')[tabIndex - 1]
         var selectedTabContent = document.getElementById(
             'tab-content-' + tabIndex
@@ -23,7 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
         for (var j = 0; j < tabContents.length; j++) {
             tabContents[j].style.display = 'none'
         }
-
         // Show the selected tab content
         if (selectedTabContent) {
             selectedTabContent.style.display = 'block'
