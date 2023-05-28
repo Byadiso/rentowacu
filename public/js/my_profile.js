f0775736d5e1d38d4f172e8b664f8e1e180a1b93
unknown <byadiso@gmail.com>
2023-05-28 18:58:12 +0200

    working-style-my-profile-page

--- public/js/my_profile.js
+++ public/js/my_profile.js
@@ -41,27 +41,27 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    function changeTab(tabIndex) {
-        var selectedTab = document.getElementsByClassName('tab')[tabIndex - 1]
-        var selectedTabContent = document.getElementById(
-            'tab-content-' + tabIndex
-        )
+    // function changeTab(tabIndex) {
+    //     var selectedTab = document.getElementsByClassName('tab')[tabIndex - 1]
+    //     var selectedTabContent = document.getElementById(
+    //         'tab-content-' + tabIndex
+    //     )
 
-        // Remove "active" class from all tabs
-        for (var i = 0; i < tabs.length; i++) {
-            tabs[i].classList.remove('active')
-        }
+    //     // Remove "active" class from all tabs
+    //     for (var i = 0; i < tabs.length; i++) {
+    //         tabs[i].classList.remove('active')
+    //     }
 
-        // Add "active" class to the selected tab
-        selectedTab.classList.add('active')
+    //     // Add "active" class to the selected tab
+    //     selectedTab.classList.add('active')
 
-        // Hide all tab contents
-        for (var j = 0; j < tabContents.length; j++) {
-            tabContents[j].style.display = 'none'
-        }
-        // Show the selected tab content
-        if (selectedTabContent) {
-            selectedTabContent.style.display = 'block'
-        }
-    }
+    //     // Hide all tab contents
+    //     for (var j = 0; j < tabContents.length; j++) {
+    //         tabContents[j].style.display = 'none'
+    //     }
+    //     // Show the selected tab content
+    //     if (selectedTabContent) {
+    //         selectedTabContent.style.display = 'block'
+    //     }
+    // }
 })
